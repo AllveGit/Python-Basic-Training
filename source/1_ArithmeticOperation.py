167be83c417623aea6df488e5fedd850b561e364
AllveGit <allvegamedevelop@gmail.com>
2022-04-21 19:00:35 +0900

    Initial commit

---
+++ source/1_ArithmeticOperation.py
@@ -0,0 +1,30 @@
+"""
+===== 숫자 자료형 및 사칙연산 =====
+"""
+
+print("=" * 50)
+print("Training_1. Arithmetic Operation")
+
+a = 7
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+# 몫 연산자 //
+print(a // b)
+# 나머지 연산자 %
+print(a % b)
+# a의 b제곱 연산자 **
+print(a ** b)
+
+# 주석을 길게 쓸 때는 이렇게 쓰면 된다.
+
+"""
+Hi
+Hi
+Hi
+"""
+
+print("=" * 50)
